d7669076cddfaa788be8b7b9d728d85edc0326ba
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-11-28 13:52:45 +0000

    git init bruv

---
+++ src/components/Comments.tsx
@@ -0,0 +1,18 @@
+export default async function Comments({ postSlug }: { postSlug: string }) {
+  return (
+    <div>
+      <h2>| Comments |</h2>
+      <h3>Leave a comment:</h3>
+
+      <form action={`/api/comments/${postSlug}`} method="POST">
+        <label htmlFor="username">Name:</label>
+        <input type="text" name="username" />
+
+        <label htmlFor="comment">Your comment:</label>
+        <textarea name="comment" cols={30} rows={10} />
+
+        <button type="submit">send comment</button>
+      </form>
+    </div>
+  );
+}
