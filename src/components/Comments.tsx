b2bcc53cc1b3b131c2c5e74bb34250fe294351c4
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-11-29 09:56:30 +0000

    update code to vercel

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,13 +1,12 @@
-export default async function Comments({ postSlug }: { postSlug: string }) {
-  const WEBSITE_URL = "http://localhost:3000";
+import { WEB_SITE } from "config";
 
+export default async function Comments({ postSlug }: { postSlug: string }) {
   let comments = [];
 
   try {
-    const commentsResult = await fetch(
-      `${WEBSITE_URL}/api/comments/${postSlug}`,
-      { next: { revalidate: 5 } }
-    );
+    const commentsResult = await fetch(`${WEB_SITE}/api/comments/${postSlug}`, {
+      next: { revalidate: 5 },
+    });
     const response = await commentsResult.json();
     console.log(response);
     comments = response.comments.rows;
